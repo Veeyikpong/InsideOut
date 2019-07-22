0cd59fcb590786290d3fd6740d163716ab4e2374
Pong <pong@goquo.com>
2019-07-22 17:17:17 +0800

    Optimize imports

--- app/src/androidTest/java/com/veeyikpong/insideout/GeofenceAreaTest.kt
+++ app/src/androidTest/java/com/veeyikpong/insideout/GeofenceAreaTest.kt
@@ -1,24 +1,18 @@
 package com.veeyikpong.insideout
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.veeyikpong.insideout.activity.MainActivity
 import androidx.test.rule.ActivityTestRule
+import com.veeyikpong.insideout.activity.MainActivity
 import com.veeyikpong.insideout.utils.AppConstants
-
-import org.junit.Test
-import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
